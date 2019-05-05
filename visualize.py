3d9d78d77455a8ab9e221cd068d6768960573373
Tobias Steidle <tobias.steidle@softwaredev.de>
2019-05-05 10:38:34 +0200

    feat: updated visualization.

--- visualize.py
+++ visualize.py
@@ -1,7 +1,23 @@
 import librosa, librosa.display
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from utils import *
 
 def plot_mfcc(mfcc):
     plt.figure(figsize=(25, 5))
     librosa.display.specshow(mfcc, x_axis='time')
     plt.colorbar()
+
+def plot_dataset(dataset):
+    plt.figure(figsize=(25, 5))
+    plt.plot(dataset)
+
+def plot_testfile(file_dir, n_samples=1):
+    test_files = resolve_csv_files(file_dir)
+    for i in range(n_samples):
+        data  = pd.read_csv(test_files[i], dtype={'acoustic_data': np.float32})
+        fig = plt.figure(figsize=(25, 5))
+        ax = fig.add_subplot(111)
+        data.plot(ax=ax, legend=True)
+
